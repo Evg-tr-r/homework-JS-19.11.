0c1f0f1e1aef9bdd0da6a2fdaac2966a79f8574d
Evg-tr-r <116937894+Evg-tr-r@users.noreply.github.com>
2022-11-18 15:36:39 +0300

    Циклы while и for 1.

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-let num =2;
-switch (num) {
-    case 1:
-        console.log('winter');
-        break;
-        case 2:
-            console.log('spring');
-            break;
-            case 3:
-                console.log('summer');
-                break;
-                case 4:
-                    console.log('autmn');
-                    break;
-    default :
-    console.log('default');
-}
-
+let i=0;
+const lastNumber=100;
+let message= '';
+while (i<lastNumber){
+    i++ ;
+   document.write (i+'<br>')
+} console.log(message)
 
 
 
