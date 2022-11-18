097f13d7b79399ea3a45881a7ab9d942c984ff2f
Evg-tr-r <116937894+Evg-tr-r@users.noreply.github.com>
2022-11-18 11:35:14 +0300

    2.1 short variant

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 
-const a='1';
-if (a=='1'){
-    console.log ('Верно');
-} else {
-    console.log ('Нeверно');
-}
-//
+
 const test=true;
-if (test==true){
-    console.log ('Верно');
-} else {
-    console.log ('Нeверно');
-}
+console.log(test==true);
