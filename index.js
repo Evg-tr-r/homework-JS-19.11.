9048ae64a3b242ef21591a9f639921d2d3f7d7dc
Evg-tr-r <116937894+Evg-tr-r@users.noreply.github.com>
2022-11-18 14:00:45 +0300

    switch-case

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+let num =2;
+switch (num) {
+    case 1:
+        console.log('winter');
+        break;
+        case 2:
+            console.log('spring');
+            break;
+            case 3:
+                console.log('summer');
+                break;
+                case 4:
+                    console.log('autmn');
+                    break;
+    default :
+    console.log('default');
+}
 
-let a=5;
-if (a>0&&a<5);
-console.log('Верно');
+
+
 
 
 
